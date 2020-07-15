032c4d93209014f6265f33fc78443a64f686f313
Ahmad Fakhrul <affauzi06@gmail.com>
2020-07-15 16:23:58 +0700

    route user register

--- routes/user/user.js
+++ routes/user/user.js
@@ -2,6 +2,7 @@ const user = require('../../controllers/user');
 
 module.exports = function (router) {
   router.get('/', user.findAll);
-  router.get('/:user_id', user.findOne);
+  router.get('/:id', user.findOne);
   router.put('/', user.create);
+  router.delete('/:id', user.delete);
 };

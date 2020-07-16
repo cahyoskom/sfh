69d741cd84f635cd85ff2b65dd08a6d0e805aeed
gianmalau <gianluca.biata@ui.ac.id>
2020-07-16 17:48:16 +0700

    create, edit, delete pada table class dan school. sudah ganti status juga pada class jika delete school

--- controllers/school.js
+++ controllers/school.js
@@ -1,21 +1,24 @@
-const m_class = require('../models/m_class');
+
 const { sha256 } = require('../common/sha');
 const query = require('../models/query');
 const { Op } = require('sequelize');
 const moment = require('moment');
 var config = require('../config/app.config');
+const m_school = require('../models/m_school');
+const m_class = require('../models/m_class');
+const SCHOOL_STATUS = require('../enums/status.enums');
 const CLASS_STATUS = require('../enums/status.enums');
 
 exports.findAll = async function (req, res) {
-  const model_class = m_class();
-  var data = await model_class.findAll();
+  const model_school = m_school();
+  var data = await model_school.findAll();
 
   res.json({ data: data });
 };
 
 exports.findOne = async function (req, res) {
-  const model_class = m_class();
-  var datum = await model_class.findOne({
+  const model_school = m_school();
+  var datum = await model_school.findOne({
     where: { id: req.params.id }
   });
 
@@ -23,18 +26,21 @@ exports.findOne = async function (req, res) {
 };
 
 exports.create = async function (req, res) {
-  const model_class = m_class();
+  const model_school = m_school();
   var new_obj = {
     m_school_id: req.body.m_school_id,
     code: req.body.code,
     name: req.body.name,
-    description: req.body.description,
+    address: req.body.address,
+    zipcode: req.body.zipcode,
+    phone: req.body.phone,
+    avatar: req.body.avatar,
     status: 1,
     created_date: moment().format(),
     created_by: req.body.name
   };
   try {
-    var datum = await model_class.create(new_obj);
+    var datum = await model_school.create(new_obj);
     res.json({ data: datum });
   } catch (err) {
     res.status(411).json({ error: 11, message: err.message });
@@ -42,18 +48,21 @@ exports.create = async function (req, res) {
 };
 
 exports.update = async function (req, res) {
-  const model_class = m_class();
+  const model_school = m_school();
   var update_obj = {
     m_school_id: req.body.m_school_id,
     code: req.body.code,
     name: req.body.name,
-    description: req.body.description,
-    status: req.body.status,
+    address: req.body.address,
+    zipcode: req.body.zipcode,
+    phone: req.body.phone,
+    avatar: req.body.avatar,
+    status: SCHOOL_STATUS.ACTIVE,
     updated_date: moment().format(),
     updated_by: req.body.name
   };
   try {
-    var datum = await model_class.update(update_obj, {
+    var datum = await model_school.update(update_obj, {
       where: { id: req.params.id }
     });
     res.json({ message: 'Data has been updated.' });
@@ -63,11 +72,16 @@ exports.update = async function (req, res) {
 };
 
 exports.delete = async function (req, res) {
+  const model_school = m_school();
   const model_class = m_class();
-  model_class.update(
-    { status: CLASS_STATUS.DELETED },{ where: { id: req.params.id } }
-    
+  model_school.update(
+    { status: SCHOOL_STATUS.DELETED },{ where: { id: req.params.id } }
   );
+  model_class.update(
+    { status: CLASS_STATUS.DELETED },{ where: { m_school_id: req.body.m_school_id } }
+  );
+
+
 
   res.json({ message: 'Data has been deleted.' });
 };
---
+++ routes/school/school.js
@@ -0,0 +1,10 @@
+const school = require('../../controllers/school');
+
+module.exports = function (router) {
+  router.get('/', school.findAll);
+  router.get('/:id', school.findOne);
+  router.post('/:id', school.update);
+  router.put('/', school.create);
+  router.delete('/:id', school.delete);
+  
+};

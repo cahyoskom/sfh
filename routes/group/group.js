ee57b525fbd63fd16293ec9db7ea1d7234eb3102
Afa <ifundeasy@gmail.com>
2020-07-20 19:17:44 +0700

    add missing enums, enable group.create

--- routes/group/group.js
+++ routes/group/group.js
@@ -3,5 +3,5 @@ const group = require('../../controllers/group');
 module.exports = function (router) {
   router.get('/', group.findAll);
   router.get('/:id', group.findOne);
-  // router.put('/', group.create);
+  router.put('/', group.create);
 };

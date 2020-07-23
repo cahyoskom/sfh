462ac35399fbc997706fc292390c8ba791e43616
Afa <ifundeasy@gmail.com>
2020-07-24 00:25:52 +0700

    improve project properties

---
+++ ecosystem.config.js
@@ -0,0 +1,45 @@
+const { env } = process;
+const { name, repository } = require('./package.json');
+const host = env.DEPLOY_HOST ? env.DEPLOY_HOST.trim() : '';
+const user = env.DEPLOY_USER ? env.DEPLOY_USER.trim() : '';
+const path = `/home/${user}/${name}`;
+const repo = repository.url.replace('git+ssh://', '');
+
+module.exports = {
+  apps: [
+    {
+      name,
+      script: 'index.js',
+      env: {
+        PORT: 30200,
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        PORT: 30100,
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy: {
+    development: {
+      user,
+      host,
+      path,
+      repo,
+      ref: 'origin/dev',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy':
+        'npm install && pm2 startOrRestart ecosystem.config.js --env development && pm2 save'
+    },
+    production: {
+      user,
+      host,
+      path,
+      repo,
+      ref: 'origin/master',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'post-deploy':
+        'npm install && pm2 startOrRestart ecosystem.config.js --env production && pm2 save'
+    }
+  }
+};

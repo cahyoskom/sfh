e80c2a6133f4a8e6ba2cf5b5e65cc3da337a4c1e
Afa <ifundeasy@gmail.com>
2020-07-24 15:41:34 +0700

    purpose using PORT 3001 and backend using 3000

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,14 +1,14 @@
-// export const API_BASE_URL_DEV = "http://144.91.77.116:3000"
-export const API_BASE_URL_DEV = "http://localhost:5000";
+// export const API_BASE_URL_DEV = "http://144.91.77.116:3001"
+export const API_BASE_URL_DEV = 'http://localhost:3000';
 // export const API_BASE_URL_DEV = "https://karpadev.karpalabs.com/sfhapi"
 
 export const API_PATH = {
-  login: "/login",
-  table: "/table",
-  logout: "/logout",
-  newPassword: "/forgot_password",
-  updatePassword: "/update_password/",
-  oauth: "/login_google",
-  register: "/registration",
-  requestActivation: "/request_activation",
+  login: '/login',
+  table: '/table',
+  logout: '/logout',
+  newPassword: '/forgot_password',
+  updatePassword: '/update_password/',
+  oauth: '/login_google',
+  register: '/registration',
+  requestActivation: '/request_activation'
 };

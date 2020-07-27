5715c5b71ffde7631c540d873cae7c7ad48cee33
Afa <ifundeasy@gmail.com>
2020-07-27 21:06:49 +0700

    revert ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,9 @@
 const { env } = process;
+const { name, repository } = require('./package.json');
 const host = env.DEPLOY_HOST ? env.DEPLOY_HOST.trim() : '';
 const user = env.DEPLOY_USER ? env.DEPLOY_USER.trim() : '';
-const mode = env.CI_ENVIRONMENT_NAME
-  ? env.CI_ENVIRONMENT_NAME.trim()
-  : 'development';
-const suffix = mode.substr(0, mode.toLowerCase() == 'development' ? 3 : 4);
-const name =
-  (env.CI_PROJECT_TITLE ? env.CI_PROJECT_TITLE.trim() : 'none') + '-' + suffix;
 const path = `/home/${user}/${name}`;
-const repo = `git@gitlab.com:${env.CI_PROJECT_PATH}.git`;
+const repo = repository.url.replace('git+ssh://', '');
 
 console.log(env);
 console.log({ user, host, path, repo });

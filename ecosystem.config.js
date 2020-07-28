deaf9d99e96a06e621e2449180e1999bf92fd2d8
Afa Rappresent <mail.vudin@gmail.com>
2020-07-28 12:41:27 +0700

    fix deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,27 @@ const { env } = process;
 const { name, repository } = require('./package.json');
 const host = env.DEPLOY_HOST ? env.DEPLOY_HOST.trim() : '';
 const user = env.DEPLOY_USER ? env.DEPLOY_USER.trim() : '';
-const path = `/home/${user}/${name}`;
+const path = env.DEPLOY_PATH ? env.DEPLOY_PATH.trim() : '';
 const repo = repository.url.replace('git+ssh://', '');
 
 console.log(env);
-console.log({ user, host, path, repo });
 
 module.exports = {
   apps: [
     {
-      name,
+      name, //todo: need toggling logic: sfh-api-prod or sfh-api-dev for pm2 name
       script: 'http.js',
+      // instances: 4,
+      // exec_mode: 'cluster',
+      exec_interpreter: 'node',
+      autorestart: true,
+      max_memory_restart: '1G',
+      merge_logs: true,
+      error_file: './log/pm2.err',
+      out_file: './log/pm2.out',
+      log_file: './log/pm2.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      pid_file: './pm2.pid',
       env: {
         PORT: 30200,
         NODE_ENV: 'development'
@@ -27,22 +37,21 @@ module.exports = {
     development: {
       user,
       host,
-      path,
       repo,
+      path,
       ref: 'origin/dev',
       ssh_options: 'StrictHostKeyChecking=no',
-      'post-deploy':
-        'npm install && pm2 startOrRestart ecosystem.config.js --env development && pm2 save'
+      'post-deploy': `mv ~/.env . && git pull && npm install && pm2 startOrRestart ecosystem.config.js --env development && pm2 save`
     },
     production: {
       user,
       host,
-      path,
       repo,
+      path,
       ref: 'origin/master',
       ssh_options: 'StrictHostKeyChecking=no',
       'post-deploy':
-        'npm install && pm2 startOrRestart ecosystem.config.js --env production && pm2 save'
+        'mv ~/.env . && git pull && npm install && pm2 startOrRestart ecosystem.config.js --env production && pm2 save'
     }
   }
 };
--- http.js
+++ http.js
@@ -4,7 +4,6 @@
  * Module dependencies.
  */
 require('dotenv').config();
-console.log(JSON.stringify(process.env, 0, 2));
 const app = require('./app');
 const { name } = require('./package.json');
 const debug = require('debug')(`${name}:server`);

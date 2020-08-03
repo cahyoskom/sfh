993ff470166064b43b7eb95d48d0140a9f6b8410
Ahmad Fakhrul <affauzi06@gmail.com>
2020-08-03 18:14:50 +0700

    api_path profile

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,14 +1,15 @@
-// export const API_BASE_URL_DEV = 'http://localhost:3000';
-export const API_BASE_URL_DEV = 'https://karpadev.karpalabs.com/sfhapi-dev';
+export const API_BASE_URL_DEV = "http://localhost:3000";
+//export const API_BASE_URL_DEV = 'https://karpadev.karpalabs.com/sfhapi-dev';
 
 export const API_PATH = {
-  login: '/login',
-  table: '/table',
-  logout: '/logout',
-  newPassword: '/forgot_password',
-  updatePassword: '/update_password/',
-  oauth: '/login_google',
-  register: '/registration',
-  requestActivation: '/request_activation',
-  activating: '/activating'
+  login: "/login",
+  table: "/table",
+  logout: "/logout",
+  newPassword: "/forgot_password",
+  updatePassword: "/update_password/",
+  oauth: "/login_google",
+  register: "/registration",
+  requestActivation: "/request_activation",
+  activating: "/activating",
+  profile: "/profile/:id",
 };

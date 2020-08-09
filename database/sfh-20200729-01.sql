d87052ee296de6cf0c933bca5dc9ab058a947d16
Afa Rappresent <mail.vudin@gmail.com>
2020-08-09 09:07:12 +0700

    add note alter sql

--- database/sfh-20200729-01.sql
+++ database/sfh-20200729-01.sql
@@ -1,2 +1,3 @@
-ALTER TABLE `m_class` CHANGE `m_school_id` `m_school_id` INT(11)  UNSIGNED  NULL;
-ALTER TABLE `sec_user` ADD `is_adim` TINYINT(4)  NOT NULL  DEFAULT '0'  AFTER `auth_data`;
+ALTER TABLE `m_class` CHANGE `m_school_id` `m_school_id` INT(11) UNSIGNED NULL;
+ALTER TABLE `sec_user` ADD `is_admin` TINYINT(4) NOT NULL DEFAULT '0' AFTER `auth_data`;
+ALTER TABLE `m_class` ADD `note` VARCHAR(200) NULL DEFAULT NULL AFTER `avatar`;

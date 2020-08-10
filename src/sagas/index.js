bbf37667df5851b11887ce040820447f087b4a26
Ahmad Fakhrul <affauzi06@gmail.com>
2020-08-10 08:49:12 +0700

    add saga updateProfile

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,7 +5,8 @@ import taskSiswa from './tasksiswa';
 import taskOrtu from './taskortu';
 import taskKepsek from './taskkepsek';
 import taskGuru from './taskguru';
+import profile from './profile';
 
 export default function* rootSaga(getState) {
-  yield all([account(), taskSiswa(), taskOrtu(), taskKepsek(), taskGuru(), admin()]);
+  yield all([account(), taskSiswa(), taskOrtu(), taskKepsek(), taskGuru(), admin(), profile()]);
 }
--- src/sagas/profile.js
+++ src/sagas/profile.js
@@ -1,31 +1,70 @@
 import { all, takeEvery, put, fork, select, call } from 'redux-saga/effects';
-import { REQUEST_USER_DATA } from '../constants/ActionTypes';
+import {
+  REQUEST_USER_DATA,
+  GET_USER_DATA,
+  SET_PROFILE_SUCCESS,
+  SET_PROFILE_FAILED,
+  SET_PROFILE
+} from '../constants/ActionTypes';
 import { fail, success } from '../components/common/toast-message';
 import * as services from '../services';
 import { API_BASE_URL_DEV, API_PATH } from '../constants/api';
 import { Header, HeaderAuth } from '../services/header';
 
-const getProfileState = state => state.profile;
-export function* requestUserData(action) {
-  console.log('a');
+const getAccountState = state => state.account;
+export function* requestUserData() {
   try {
-    const profileState = yield select(getProfileState);
-    const getProfile = profileState.profile;
-
-    let param = { id: getProfile.id };
-    console.log(param);
-
     const reqUserData = yield call(services.GET, API_BASE_URL_DEV + API_PATH.profile, HeaderAuth());
-    console.log(reqUserData);
+    //console.log(reqUserData);
+    console.log(reqUserData.data.data);
+
     yield put({
       type: GET_USER_DATA,
-      value: reqUserData.data
+      value: reqUserData.data.data
     });
+    // yield put({
+    //   type: GET_USER_DATA,
+    //   data: reqUserData.value
+    // });
   } catch (e) {
     // yield put(e);
   }
 }
 
+export function* updateProfile() {
+  try {
+    const accountState = yield select(getAccountState);
+    const profileState = accountState.profile;
+    let param = {
+      email: profileState.email,
+      password: profileState.password,
+      phone: profileState.phone,
+      name: profileState.name
+    };
+    const _response = yield call(services.PUT, API_BASE_URL_DEV + API_PATH.profile, param, HeaderAuth());
+    if (_response.status === 200) {
+      yield put({
+        type: SET_PROFILE_SUCCESS
+      });
+    } else if (_response.data.error === 401001) {
+      yield put({
+        type: SET_PROFILE_FAILED,
+        value: _response.data.message
+      });
+    } else {
+      yield put({
+        type: SET_PROFILE_FAILED,
+        value: _response.data.message
+      });
+    }
+  } catch (error) {
+    yield put({
+      type: SET_PROFILE_FAILED,
+      value: error
+    });
+  }
+}
+
 export default function* rootSaga() {
-  yield all([takeEvery(REQUEST_USER_DATA, requestUserData)]);
+  yield all([takeEvery(REQUEST_USER_DATA, requestUserData), takeEvery(SET_PROFILE, updateProfile)]);
 }

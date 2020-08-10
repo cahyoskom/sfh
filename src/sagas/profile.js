b7dba649c606710bc39e8e7d5ca62a87d90e4949
Ahmad Fakhrul <affauzi06@gmail.com>
2020-08-10 17:55:23 +0700

    update sagas profile

--- src/sagas/profile.js
+++ src/sagas/profile.js
@@ -4,28 +4,27 @@ import {
   GET_USER_DATA,
   SET_PROFILE_SUCCESS,
   SET_PROFILE_FAILED,
-  SET_PROFILE
+  SET_PROFILE,
+  ON_CHANGE_STATE_EDIT_PROFILE
 } from '../constants/ActionTypes';
 import { fail, success } from '../components/common/toast-message';
 import * as services from '../services';
 import { API_BASE_URL_DEV, API_PATH } from '../constants/api';
 import { Header, HeaderAuth } from '../services/header';
+import { data } from 'jquery';
 
-const getAccountState = state => state.account;
+const getProfileState = state => state.profile;
+
 export function* requestUserData() {
   try {
     const reqUserData = yield call(services.GET, API_BASE_URL_DEV + API_PATH.profile, HeaderAuth());
     //console.log(reqUserData);
-    console.log(reqUserData.data.data);
+    console.log(reqUserData.data);
 
     yield put({
       type: GET_USER_DATA,
-      value: reqUserData.data.data
+      value: reqUserData.data[0]
     });
-    // yield put({
-    //   type: GET_USER_DATA,
-    //   data: reqUserData.value
-    // });
   } catch (e) {
     // yield put(e);
   }
@@ -33,15 +32,33 @@ export function* requestUserData() {
 
 export function* updateProfile() {
   try {
-    const accountState = yield select(getAccountState);
-    const profileState = accountState.profile;
+    const profileState = yield select(getProfileState);
+    const profileInfoState = profileState.data;
+    const profileEditState = profileState.profile;
     let param = {
-      email: profileState.email,
-      password: profileState.password,
-      phone: profileState.phone,
-      name: profileState.name
+      email: profileEditState.email,
+      password: profileEditState.password,
+      phone: profileEditState.phone,
+      name: profileEditState.name
     };
-    const _response = yield call(services.PUT, API_BASE_URL_DEV + API_PATH.profile, param, HeaderAuth());
+
+    if (param.name == '') {
+      yield put({
+        type: ON_CHANGE_STATE_EDIT_PROFILE,
+        value: 'Nama tidak boleh kosong',
+        field: 'errormsg'
+      });
+      yield put({
+        type: ON_CHANGE_STATE_EDIT_PROFILE,
+        value: true,
+        field: 'openAlert'
+      });
+      return;
+    }
+
+    console.log(param);
+
+    const _response = yield call(services.POST, API_BASE_URL_DEV + API_PATH.profile, param, HeaderAuth());
     if (_response.status === 200) {
       yield put({
         type: SET_PROFILE_SUCCESS

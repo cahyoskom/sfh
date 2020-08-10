d88072ce6ed6b5486306ca7f6387230d25c7803f
Ahmad Fakhrul <affauzi06@gmail.com>
2020-08-10 08:49:27 +0700

    update reducer profile

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -1,12 +1,24 @@
-import { GET_USER_DATA, REQUEST_USER_DATA } from "../constants/ActionTypes";
+import {
+  GET_USER_DATA,
+  SET_PROFILE_FAILED,
+  SET_PROFILE_SUCCESS,
+  ON_CHANGE_STATE_PROFILE,
+  REQUEST_USER_DATA
+} from '../constants/ActionTypes';
 
 const initialState = {
-  data: {
-    name: "Fakhrul",
-    email: "affauzi06@gmail.com",
-    phone: "082113369110",
-    avatar: "",
-  },
+  data: {},
+  profile: {
+    isChecked: false,
+    name: '',
+    email: '',
+    password: '',
+    rePassword: '',
+    phone: '',
+    showErrorProfile: false,
+    errorMessage: '',
+    success: false
+  }
 };
 
 export default function profileReducer(state = initialState, action) {
@@ -14,7 +26,32 @@ export default function profileReducer(state = initialState, action) {
     case GET_USER_DATA:
       return {
         ...state,
-        data: action.value,
+        data: action.value
+      };
+    case SET_PROFILE_FAILED:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          errorMessage: action.value,
+          showErrorProfile: true
+        }
+      };
+    case SET_PROFILE_SUCCESS:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          success: true
+        }
+      };
+    case ON_CHANGE_STATE_PROFILE:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          [action.field]: action.value
+        }
       };
     default:
   }

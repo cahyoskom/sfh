4801bcc943096ab42e2d648fb2df1ce4b86139ff
Fivi Melinda <fivimelinda@gmail.com>
2020-08-12 16:35:02 +0700

    add feature get member, change owner, remove member, add member

--- routes/school/school.js
+++ routes/school/school.js
@@ -3,6 +3,7 @@ const school = require('../../controllers/school');
 module.exports = function (router) {
   router.get('/', school.findAll);
   router.get('/class_list', school.getAllClass);
+  router.get('/members/:id', school.getMembers);
   router.get('/:id', school.findOne);
   router.post('/', school.update);
   router.put('/', school.create);
@@ -10,4 +11,7 @@ module.exports = function (router) {
   router.post('/connect_class', school.connectClass);
   router.put('/create_class', school.createClass);
   router.post('/approval/:classId', school.approval);
+  router.post('/change_owner', school.changeOwner);
+  router.post('/remove_member', school.removeMember);
+  router.post('/add_member', school.inviteMember);
 };

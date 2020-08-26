3a0a4d7d747164ce8540381392353907253e8f52
Ahmad Fakhrul <affauzi06@gmail.com>
2020-08-26 15:14:19 +0700

    update route and controller audit

--- controllers/audit.js
+++ controllers/audit.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const log_audit = require('../models/log_audit');
 const { Op } = require('sequelize');
 
@@ -5,47 +6,63 @@ exports.getData = async function (req, res) {
   const model_audit = log_audit();
   var audit = await model_audit.findAll();
 
-  console.log('Test Audit');
-  res.json({ data: audit });
-};
+  console.log('start_Date === ' + moment(req.query.start_date).format());
+  console.log('end_Date === ' + moment(req.query.end_date).format());
+  console.log('Email === ' + req.query.email);
+  console.log('Action === ' + req.query.action);
 
-exports.getFilter = async function (req, res) {
-  const model_audit = log_audit();
-
-  console.log(req.query);
-
-  if (req.query.action && req.query.email && req.query.date) {
+  if (req.query.action && req.query.email && req.query.start_date && req.query.end_date) {
     var audit = await model_audit.findAll({
       where: {
         [Op.and]: [
           { action: req.query.action },
           { sec_user_email: { [Op.like]: req.query.email } },
-          { action_date: req.query.date }
+          {
+            action_date: {
+              [Op.between]: [
+                moment(req.query.start_date).add(1, 'day'),
+                moment(req.query.end_date).add(1, 'day')
+              ]
+            }
+          }
         ]
       }
     });
   } else if (req.query.action && req.query.email) {
     var audit = await model_audit.findAll({
       where: {
+        [Op.and]: [{ action: req.query.action }, { sec_user_email: { [Op.like]: req.query.email } }]
+      }
+    });
+  } else if (req.query.action && req.query.start_date && req.query.end_date) {
+    var audit = await model_audit.findAll({
+      where: {
         [Op.and]: [
           { action: req.query.action },
-          { sec_user_email: { [Op.like]: req.query.email } }
-          //{ action_date: req.query.date }
+          {
+            action_date: {
+              [Op.between]: [
+                moment(req.query.start_date).add(1, 'day'),
+                moment(req.query.end_date).add(1, 'day')
+              ]
+            }
+          }
         ]
       }
     });
-  } else if (req.query.action && req.query.date) {
-    var audit = await model_audit.findAll({
-      where: {
-        [Op.and]: [{ action: req.query.action }, { action_date: req.query.date }]
-      }
-    });
-  } else if (req.query.email && req.query.date) {
+  } else if (req.query.email && req.query.start_date && req.query.end_date) {
     var audit = await model_audit.findAll({
       where: {
         [Op.and]: [
           { sec_user_email: { [Op.like]: req.query.email } },
-          { action_date: req.query.date }
+          {
+            action_date: {
+              [Op.between]: [
+                moment(req.query.start_date).add(1, 'day'),
+                moment(req.query.end_date).add(1, 'day')
+              ]
+            }
+          }
         ]
       }
     });
@@ -57,11 +74,19 @@ exports.getFilter = async function (req, res) {
     var audit = await model_audit.findAll({
       where: { sec_user_email: { [Op.like]: req.query.email } }
     });
-  } else if (req.query.date) {
+  } else if (req.query.start_date && req.query.end_date) {
     var audit = await model_audit.findAll({
-      where: { action_date: req.query.date }
+      where: {
+        action_date: {
+          [Op.between]: [
+            moment(req.query.start_date).add(1, 'day'),
+            moment(req.query.end_date).add(1, 'day')
+          ]
+        }
+      }
     });
   }
 
+  console.log('Test Audit');
   res.json({ data: audit });
 };
--- routes/audit/audit.js
+++ routes/audit/audit.js
@@ -2,5 +2,4 @@ const audit = require('../../controllers/audit');
 
 module.exports = function (router) {
   router.get('/', audit.getData);
-  router.get('/filter', audit.getFilter);
 };

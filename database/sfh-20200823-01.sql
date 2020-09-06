21aaca8672c93e989d1e4f95700820eeea50ddac
Fivi Melinda <fivimelinda@gmail.com>
2020-09-06 16:47:14 +0700

    change school avatar type into long text

--- database/sfh-20200823-01.sql
+++ database/sfh-20200823-01.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `t_class` ADD UNIQUE(`code`);
 ALTER TABLE `t_school` ADD UNIQUE(`code`); 
 ALTER TABLE `t_class_task_file` ADD `link` VARCHAR(1000) NULL DEFAULT NULL AFTER `sequence`; 
+ALTER TABLE `t_school` CHANGE `avatar` `avatar` LONGTEXT CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL; 

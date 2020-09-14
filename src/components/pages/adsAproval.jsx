9cc624fa7e0b13e4d262612960151e0d1d7a0cb1
R. Cahyo Tri P <cahyo_skom@yahoo.com>
2020-09-14 08:21:05 +0700

    Ads List + Ads Aproval UI

--- src/components/pages/adsAproval.jsx
+++ src/components/pages/adsAproval.jsx
@@ -48,16 +48,33 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
 
-
-class AdsList extends Component {
+class AdsAproval extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      modal: false,
+      msg: "",
       table: {
         maxWidth: 200
       }
     };
   }
+  onSetujuiClick = (e) => {
+    console.log(e);
+    this.setState({
+      msg: 'Apakah Anda sudah yakin akan menyetujui publikasi iklan Milo?',
+      modal: true
+    });
+  };
+  onTolakClick = () => {
+    this.setState({
+      msg: 'Apakah Anda sudah yakin akan tolak publikasi iklan Milo?',
+      modal: true
+    });
+  };
+  closeModal = () => {
+    this.setState({ modal: false });
+  };
 
 
   // componentDidMount() {
@@ -68,28 +85,9 @@ class AdsList extends Component {
   // }
 
   render() {
-    // const useStyles = makeStyles((theme) => ({
-    //   root: {
-    //     flexGrow: 1,
-    //   },
-    //   paper: {
-    //     padding: theme.spacing(2),
-    //     margin: 'auto',
-    //     maxWidth: 500,
-    //   },
-    //   image: {
-    //     width: 128,
-    //     height: 128,
-    //   },
-    //   img: {
-    //     margin: 'auto',
-    //     display: 'block',
-    //     maxWidth: '100%',
-    //     maxHeight: '100%',
-    //   },
-    // }));
 
-    const { classState, postAddMember, onChangeStateAddMember } = this.props;
+    const { buttonLabel, className, toggle, modal, closeModal } = this.props;
+
     let iklan = [
       {
         id: 1,
@@ -101,138 +99,108 @@ class AdsList extends Component {
         tarif: 5000
       }
     ];
+
+
+
+
     return (
       <>
         <div style={{ flexGrow: '1' }} name="rootpage">
+          <Grid>
+            <Paper
+              className="h5 font-weight-bold pb-2"
+              elevation={0}
+              square
+              style={{
+                margin: 'auto',
+                maxWidth: 800,
+              }}>Pratinjau
+            </Paper>
+          </Grid>
           <Paper elevated={3} variant="outlined" square style={{
             margin: 'auto',
-            maxWidth: 500,
+            maxWidth: 800,
             padding: '20px',
           }}>
+
             <Grid container spacing={1} name="Grid Main Container" justify="center" alignItem="center">
-
-              <Grid container item spacing={3} xs={12} name="Grid Container 1" >
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 1</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 2</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 3</Paper></Grid>
+              <Grid container item spacing={3} xs={12} name="Grid Container 1" justify="center" alignItem="center">
+                {/* <Grid item xs={12} ><Paper style={{ padding: '20px' }}>Iklan Atas</Paper></Grid> */}
               </Grid>
 
-              <Grid container item spacing={3} xs={12} name="Grid Container 2" >
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 1</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 2</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 3</Paper></Grid>
+              <Grid container item spacing={0} xs={12} name="Grid Container 2" justify="center" alignItem="center">
+                {/* <Grid item xs={2} ><Paper style={{ padding: '20px' }}>Iklan Kiri</Paper></Grid> */}
+                <Grid item xs={12} ><Paper elevation={0} style={{ padding: '20px 20px 0px 20px' }}>
+                  <img
+                    style={{
+                      margin: 'auto',
+                      display: 'block',
+                      maxWidth: '100%',
+                      maxHeight: '100%',
+                    }}
+                    alt="Main Item Tengah"
+                    src={`${process.env.PUBLIC_URL}/assets/images/layout-item-1-iklan.png`}
+                  /></Paper></Grid>
+                {/* <Grid item xs={2} ><Paper style={{ padding: '20px' }}>Iklan Kanan</Paper></Grid> */}
               </Grid>
 
-              <Grid container item spacing={3} xs={12} name="Grid Container 3" >
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 1</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 2</Paper></Grid>
-                <Grid item><Paper xs={4} style={{ padding: '20px' }}>Iklan Bawah 3</Paper></Grid>
+              <Grid container item spacing={3} xs={12} name="Grid Container 3" justify="center" alignItem="center">
+                <Grid item xs={12} ><Paper elevation={0} style={{ padding: '0px 15px 0px 15px' }}><img
+                  style={{
+                    margin: 'auto',
+                    display: 'block',
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                  }}
+                  alt="Iklan Bawah"
+                  src={`${process.env.PUBLIC_URL}/assets/images/milo.jpg`}
+                /></Paper></Grid>
               </Grid>
-
             </Grid>
-
-
-
           </Paper>
-
+          <Grid>
+            <Paper
+              elevation={0}
+              square
+              style={{
+                margin: 'auto',
+                maxWidth: 800,
+                padding: '20px 0px 0px 0px',
+              }}>
+              <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+                <div class="btn-group" role="group" aria-label="1st group">
+                  <Link to={`${process.env.PUBLIC_URL}`}>
+                    <Button size="small" variant="contained" className="">Kembali</Button>
+                  </Link>
+                </div>
+                <div class="btn-group" role="group" aria-label="2nd group">
+                  <Button size="small" variant="contained" color="primary" onClick={this.onSetujuiClick}>Setujui</Button>
+                  <Button size="small" variant="contained" color="secondary" className="ml-3" onClick={this.onTolakClick}>Tolak</Button>
+                </div>
+              </div>
+            </Paper>
+          </Grid>
         </div >
         <div>&nbsp;</div>
+        {/* modal Setujui Ads */}
+        <Modal
+          isOpen={this.state.modal}
+          centered={true}
+          style={{
+            maxWidth: 400,
+          }}>
+          <ModalBody>
+            <div className="text-center m-3">{this.state.msg}</div>
+            <div className="text-center m-2 mt-3">
+              <Button variant="contained" className="mr-3" onClick={this.closeModal}>Kembali</Button>
+              <Button variant="contained" color="primary" onClick={this.closeModal}>Ya, sudah yakin</Button>{' '}
+            </div>
+          </ModalBody>
+        </Modal>
       </>
     );
   }
 }
 
 
-export default connect(/* mapStateToProps, mapDispatchToProps */)(AdsList);
-
-// <Grid item xs={10} sm={10} md={10} lg={10} xl={12} spacing={1}>
-//   <div className="h5 font-weight-bold pb-3">Pratinjau</div>
-// </Grid>
-//   <Grid></Grid>
-//   <Paper
-//     elevation={3}
-//   style={{
-//     margin: '0px 100px 16px 100px',
-//     padding: '50px 0px 0px 0px',
-//     maxWidth: '200',
-//     backgroundImage: `url('${process.env.PUBLIC_URL}/assets/images/layout-bgcontainer-iklan.png')`,
-//     backgroundRepeat: 'no-repeat',
-//     backgroundSize: 'cover',
-//   }}
-//   >
-
-//     <Grid
-//       item
-//       style={{
-//         margin: '75px',
-//         padding: '0px 0px 50px 0px ',
-//       }}>
-//        <img
-//                     style={{
-//                       margin: 'auto',
-//                       display: 'block',
-//                       maxWidth: '100%',
-//                       maxHeight: '100%',
-//                       width: '1200px',
-//                     }}
-//                     alt="bgcontainer"
-//                     src={`${process.env.PUBLIC_URL}/assets/images/layout-bgcontainer-iklan.png`}
-//                   />
-
-//       <Grid container direction='col' spacing={1} justify='center' alignItems='center'>
-
-//         <Grid direction='row' item alt="ads-left-position">
-//           <img
-//                           style={{
-//                             margin: 'auto',
-//                             display: 'block',
-//                             maxWidth: '100%',
-//                             maxHeight: '100%',
-//                             width: '100px',
-
-//                           }}
-//                           alt="Ads Left"
-//                           src={`${process.env.PUBLIC_URL}/assets/images/milo.jpg`}
-//                         />
-//         </Grid>
-//         <img
-//           style={{
-//             margin: 'auto',
-//             display: 'block',
-//             maxWidth: '100%',
-//             maxHeight: '100%',
-//             height: '500px',
-//           }}
-//           alt="mainlayout"
-//           src={`${process.env.PUBLIC_URL}/assets/images/layout-item-1-iklan.png`}
-//         />
-//         <Grid item alt="ads-right-position">
-//           <img
-//                           style={{
-//                             margin: 'auto',
-//                             display: 'block',
-//                             maxWidth: '100%',
-//                             maxHeight: '100%',
-//                             width: '100px',
-
-//                           }}
-//                           alt="Ads Right"
-//                           src={`${process.env.PUBLIC_URL}/assets/images/milo.jpg`}
-//                         />
-//         </Grid>
-//         <Grid item alt="ads-bottom-position">
-//           <img
-//             style={{
-//               margin: 'auto',
-//               display: 'block',
-//               maxWidth: '100%',
-//               maxHeight: '100%',
-//               width: '1000px',
-//             }}
-//             alt="Ads Bottom"
-//             src={`${process.env.PUBLIC_URL}/assets/images/milo.jpg`}
-//           />
-//         </Grid>
-//       </Grid>
-//     </Grid>
-//   </Paper>
+export default connect(/* mapStateToProps, mapDispatchToProps */)(AdsAproval);
